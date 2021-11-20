dfc084621629d2998b9bbfc114e83eb737fa132e
Milyn <milyng.dsilva@gmail.com>
2021-11-20 14:30:02 +0530

    minor additions

---
+++ Objects/5-Constructor-property.js
@@ -0,0 +1,14 @@
+
+const circle = createCircle(1);
+//constructor functions
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function (){
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
+
+//every obj has a constructory property and that refs the fun thatw was used to create that object
