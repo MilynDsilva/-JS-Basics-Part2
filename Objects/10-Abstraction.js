5b1a4293c29f24e5f9188c3e9e98cf9007aa6b90
Milyn <milyng.dsilva@gmail.com>
2021-11-22 17:50:37 +0530

    commit

--- Objects/10-Abstraction.js
+++ Objects/10-Abstraction.js
@@ -4,17 +4,50 @@ function Circle(radius) {
     //when we get out of the function the variable gos out of the scope
     this.radius = radius;
 
-    this.defaultLocaion = {x:0,y:0};
+    let defaultLocaion = {x:0,y:0};
     
 
-    this.compluteOptimumLocation = function () {
+    let compluteOptimumLocation = function (factor) {
         //
     }
     this.draw = function (){
-        this.compluteOptimumLocation();
+        compluteOptimumLocation(0.1);
         console.log('draw');
-    }
+    };
 }
 
 const circle = new Circle(10); //this refs the new circle object
 console.log(circle.color)
+circle.draw();
+//prevernts issues later
+//closures are there  but scope is temperory , everytime a function is called
+//the varible scope is created
+
+
+
+
+
+
+
+
+
+// function Circle(radius) {
+//     //let color = 'red';
+//     //this.color = 'reds'; //local variable inside the function
+//     //when we get out of the function the variable gos out of the scope
+//     this.radius = radius;
+
+//     this.defaultLocaion = {x:0,y:0};
+    
+
+//     this.compluteOptimumLocation = function () {
+//         //
+//     }
+//     this.draw = function (){
+//         this.compluteOptimumLocation();
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(10); //this refs the new circle object
+// console.log(circle.color)
