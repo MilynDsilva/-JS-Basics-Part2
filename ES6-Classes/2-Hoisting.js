babdb5a17307cc418efaa47d2c2d11b6e807596e
Milyn <milyng.dsilva@gmail.com>
2021-11-25 12:26:48 +0530

    commit

---
+++ ES6-Classes/2-Hoisting.js
@@ -0,0 +1,14 @@
+sayHello(); //works
+console.log(number); //err
+
+sayGoodbye(); //error //not initialized
+
+//func decl
+function sayHello() { }
+//are hoisted // raised to top 
+
+
+//fun expression
+
+const sayGoodbye = function () { };
+const number = 1;
